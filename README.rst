4269fc6fada8c01dd21d04011ba06c31be0d3b4c
Maarten van Gompel <proycon@anaproy.nl>
2017-01-04 14:17:02 +0100

    Added setup and updated readme

--- README.rst
+++ README.rst
@@ -1,1 +1,22 @@
-A collection of CLAM webservices for various of our Natural Language Processing tools
+A collection of CLAM webservices (powered using CLAM,
+https://proycon.github.io/clam) for various of our Natural Language Processing
+tools.  These webservices are hosted on https://webservices-lst.science.ru.nl .
+
+CLAM webservices for the following tools are supplied here:
+
+* `Frog <https://languagemachines.github.io/frog>`_ 
+* `Ucto <https://languagemachines.github.io/ucto>`_ 
+* `Timbl <https://languagemachines.github.io/timbl>`_ 
+* `Colibri Core <https://proycon.github.io/colibri-core>`_ 
+
+The tools themselves are not in this repository and need to be installed
+separately, we recommend to use our `LaMachine <https://proycon.github.io/LaMachine>`_
+distribution which contains everything at once.
+
+To install these CLAM webservices manually, clone this repository and run:
+
+``python setup.py install``
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+
+from __future__ import print_function
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "CLAMServices",
+    version = "1.0", #also change in clam.common.data.VERSION
+    author = "Maarten van Gompel",
+    author_email = "proycon@anaproy.nl",
+    description = ("A collection of CLAM Webservices for various of our NLP tools"),
+    license = "GPL",
+    keywords = "clam webservice rest nlp computational_linguistics rest",
+    url = "https://proycon.github.io/clam",
+    packages=['clamservices','clamservices.wsgi','clamservices.config','clamservices.wrappers'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3", #3.0, 3.1 and 3.2 are not supported by flask
+        "Programming Language :: Python :: 3.4", #3.0, 3.1 and 3.2 are not supported by flask
+        "Programming Language :: Python :: 3.5", #3.0, 3.1 and 3.2 are not supported by flask
+        "Operating System :: POSIX",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    package_data = {'clamservices':['wrappers/*.sh','wsgi/*.wsgi'] },
+    include_package_data=True,
+    install_requires=['CLAM >= 2.1']
+)
