d92854a5b4e0dcf3002bb25f2db444ca5fd43339
Maarten van Gompel <proycon@anaproy.nl>
2019-03-23 21:37:28 +0100

    added spacy (spacy2folia) webservice

---
+++ clamservices/config/spacy.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+
+###############################################################
+# CLAM: Computational Linguistics Application Mediator
+# -- Settings --
+#       by Maarten van Gompel (proycon)
+#       http://proycon.github.io/clam/
+#       Centre for Language and Speech Technology  / Language Machines
+#       Radboud University Nijmegen
+#
+#       Licensed under GPLv3
+#
+###############################################################
+
+
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+from clam.common.parameters import *
+from clam.common.formats import *
+from clam.common.viewers import *
+from clam.common.data import *
+from clam.common.converters import *
+from clam.common.digestauth import pwhash
+import clamservices.wrappers
+import os
+import spacy
+from base64 import b64decode as D
+
+REQUIRE_VERSION = 2.4
+WRAPPERDIR = clamservices.wrappers.__path__[0]
+
+#THIS CONFIGURATION IS FOR FROG >= 0.12.10 ! OLDER VERSIONS WON'T WORK WITH IT!
+
+#============== General meta configuration =================
+SYSTEM_ID = "spacy"
+SYSTEM_NAME = "spaCy"
+SYSTEM_DESCRIPTION = "spaCy is a library for advanced Natural Language Processing. It's built on the very latest research, and was designed from day one to be used in real products. spaCy comes with pre-trained statistical models and word vectors, and currently supports tokenization for 45+ languages. It features the fastest syntactic parser in the world, convolutional neural network models for tagging, parsing and named entity recognition and easy deep learning integration. This webservice provides access to various types of linguistic enrichment for a wide variety of languages. It also support FoLiA XML output."
+
+
+USERS = None
+
+# ================ Server specific configuration for CLAM ===============
+
+DEBUG = False
+
+#load external configuration file
+loadconfig(__name__)
+
+
+#The system command (Use the variables $STATUSFILE $DATAFILE $PARAMETERS $INPUTDIRECTORY $OUTPUTDIRECTORY $USERNAME)
+COMMAND = WRAPPERDIR + "/spacywrapper.py $DATAFILE $STATUSFILE $OUTPUTDIRECTORY > $OUTPUTDIRECTORY/log"
+
+
+PROFILES = [
+    Profile(
+        InputTemplate('textinput', PlainTextFormat,"Text document",
+            StaticParameter(id='encoding',name='Encoding',description='The character encoding of the file', value='utf-8'),
+            PDFtoTextConverter(id='pdfconv',label='Convert from PDF Document'),
+            MSWordConverter(id='mswordconv',label='Convert from MS Word Document'),
+            CharEncodingConverter(id='latin1',label='Convert from Latin-1 (iso-8859-1)',charset='iso-8859-1'),
+            CharEncodingConverter(id='latin9',label='Convert from Latin-9 (iso-8859-15)',charset='iso-8859-15'),
+            multi=True,
+            extension='.txt',
+        ),
+        OutputTemplate('foliaoutput', FoLiAXMLFormat,"FoLiA Document",
+            FoLiAViewer(),
+            FLATViewer(url=FLATURL, mode='viewer'),
+            removeextensions=['.txt'],
+            extension='.folia.xml',
+            copymetadata=True,
+            multi=True,
+        ),
+    ),
+]
+
+models = []
+for lang in spacy.info()['models']:
+    for model in spacy.info(lang):
+        models.append( ( lang + "_" + model['name'] + model['description'] ) )
+
+PARAMETERS =  [
+    ('Model Selection', [
+        ChoiceParameter('model', 'Model','The spaCy model to use, determines for what language is processed and what linguistic enrichments are performed',choices=models,required=True ),
+    ]),
+]
---
+++ clamservices/wrappers/spacywrapper.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+###############################################################
+# CLAM: Computational Linguistics Application Mediator
+# -- CLAM Wrapper script, demonstrating CLAM Client API --
+#       by Maarten van Gompel (proycon)
+#       http://ilk.uvt.nl/~mvgompel
+#       Induction for Linguistic Knowledge Research Group
+#       Universiteit van Tilburg
+#
+#       Licensed under GPLv3
+#
+###############################################################
+
+
+#This script will be called by CLAM and will run with the current working directory set to the specified project directory
+
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+#general python modules:
+import sys
+import os
+
+
+#import CLAM-specific modules:
+import clam.common.data
+import clam.common.status
+import clam.common.parameters
+import clam.common.formats
+
+shellsafe = clam.common.data.shellsafe
+
+#this script takes three arguments: $DATAFILE $STATUSFILE $OUTPUTDIRECTORY
+datafile = sys.argv[1]
+statusfile = sys.argv[2]
+outputdir = sys.argv[3]
+
+
+#os.environ['PYTHONPATH'] = bindir + '/../lib/python' + str(sys.version_info.major) + '.' + str(sys.version_info.minor) + '/site-packages/frog' #Necessary for University of Tilburg servers (change or remove this in your own setup)
+
+#Obtain all data from the CLAM system (passed in $DATAFILE (clam.xml))
+clamdata = clam.common.data.getclamdata(datafile)
+
+#You now have access to all data. A few properties at your disposition now are:
+# clamdata.system_id , clamdata.project, clamdata.user, clamdata.status , clamdata.parameters, clamdata.inputformats, clamdata.outputformats , clamdata.input , clamdata.output
+
+clam.common.status.write(statusfile, "Starting...")
+
+inputfiles = []
+for i, inputfile in enumerate(clamdata.inputfiles('textinput')):
+    inputfiles.append(inputfile)
+
+
+clam.common.status.write(statusfile, "Processing ...")
+
+os.chdir(outputdir) #spacy2folia writes in currenet working directory
+os.system("spacy2folia --model " + shellsafe(clamdata['model']) + " ".join(( shellsafe(str(inputfile),) for inputfile in inputfiles )))
+if r != 0:
+    clam.common.status.write(statusfile, "Spacy returned with an error whilst processing. Aborting",100)
+    sys.exit(1)
+
+clam.common.status.write(statusfile, "Done",100)
+
+sys.exit(0) #non-zero exit codes indicate an error!
